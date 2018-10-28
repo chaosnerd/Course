a10dd24ad4b37c89507a63b90fe0d5c5fe2bfb1c
chaosnerd <mafianook@hotmail.com>
2018-10-28 21:05:56 +0700

    add router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,68 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+    <v-layout
+      wrap
+      style="height: 100%;"
+    >
+      <v-container>
+        <v-layout justify-center>
+          <v-content>
+            <HelloWorld/>
+          </v-content>
+        </v-layout>
+      </v-container>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+      <v-navigation-drawer
+        v-model="drawer"
+        :mini-variant="mini"
+        absolute
+        dark
+      >
+        <v-list class="pa-1">
+          <v-list-tile v-if="mini" @click.stop="mini = !mini">
+            <v-list-tile-action>
+              <v-icon>chevron_right</v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+
+          <v-list-tile avatar tag="div">
+            <v-list-tile-avatar>
+              <img src="https://randomuser.me/api/portraits/men/85.jpg">
+            </v-list-tile-avatar>
+
+            <v-list-tile-content>
+              <v-list-tile-title>John Leider</v-list-tile-title>
+            </v-list-tile-content>
+
+            <v-list-tile-action>
+              <v-btn icon @click.stop="mini = !mini">
+                <v-icon>chevron_left</v-icon>
+              </v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
+
+        <v-list class="pt-0" dense>
+          <v-divider light></v-divider>
+
+          <v-list-tile
+            v-for="item in items"
+            :key="item.title"
+            @click="mini!=mini"
+          >
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+    </v-layout>
+
+    
   </v-app>
 </template>
 
@@ -29,9 +74,15 @@ export default {
   components: {
     HelloWorld
   },
-  data () {
+   data () {
     return {
-      //
+      drawer: true,
+      items: [
+        { title: 'Home', icon: 'dashboard' },
+        { title: 'About', icon: 'question_answer' }
+      ],
+      mini: true,
+      right: null
     }
   }
 }
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,10 @@ import './plugins/vuetify'
 import App from './App.vue'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
-
+Vue.use(VueRouter)
 new Vue({
   render: h => h(App)
 }).$mount('#app')
